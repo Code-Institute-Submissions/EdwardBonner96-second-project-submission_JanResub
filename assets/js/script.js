2fa6dd874f779bc2cbfaee12b95df38476fe8304
EdwardBonner96 <edwardo.bonner@gmail.com>
2022-11-10 07:03:33 +0000

    Update js responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
+// Constants store two alphabets each so modulo calculation is unneeded
 const lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
 let keyAddedLowerCase = "";
 const upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let keyAddedUpperCase = "";
 
+// Caeser Cipher encoding
 function caeserEncode(inputText, keyEncode) {
+  // Shifts alphabets with key
   for (let i = 0; i < 26; i++) {
     let shiftedLowerCase = (keyEncode + i);
     keyAddedLowerCase += lowerCaseAlphabet[shiftedLowerCase];
@@ -11,7 +14,9 @@ function caeserEncode(inputText, keyEncode) {
     keyAddedUpperCase += upperCaseAlphabet[shiftedUpperCase];
   }
   let encodedText = "";
+  let inputLength = inputText.length;
   for (let i = 0; i <= inputLength; i++) {
+    // Tests if inputs are letters by changing cases
     if (inputText[i].toUpperCase() !== inputText[i].toLowerCase()) {
       let transformedLetter = lowerCaseAlphabet.indexOf(inputText[i]);
       encodedText += keyAddedLowerCase[transformedLetter];
@@ -20,15 +25,23 @@ function caeserEncode(inputText, keyEncode) {
       encodedText += keyAddedUpperCase[transformedLetter];
     }
   }
-  console.log(encodedText)
+  return [encodedText];
 }
 
-
+let manualKey = document.getElementById("manual-key-select");
+manualKey.addEventListener('click', caeserEncode);
 
 let randomKey = Math.floor((25 * Math.random() + 1));
 console.log(randomKey);
+
 let unscrambleKey = 26 - randomKey;
 console.log(unscrambleKey);
+
+function caeserEncode(event) { 
+  tester.submit();
+}
+let testerForm = document.getElementById('caeser-text');
+caeser-text.addEventListener('submit', caeserEncode);
 
 // Function clears text in input box
 function clearText() {
